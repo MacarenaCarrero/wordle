aa2ea5dca5ecafc14314804803462a58856c7623
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-04-15 20:56:03 +0200

    wordle

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+const ALL_WORDS = [
+  'zara',
+  'pelo',
+  'humo',
+  'chasqueador',
+  'zombie',
+  'atropello',
+  'ticket',
+  'infectado',
+  'hambre',
+  'accidente',
+  'vitrina',
+  'apocalipsis',
+  'supervivencia',
+  'sangre',
+  'hongo',
+  'cordyceps'
+];
