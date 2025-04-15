b2ba54fcb643fc1a59a1951c5789847833886a8f
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-04-16 00:08:38 +0200

    ...

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+const boxGameElement = document.getElementById('boxGame');
+const wordElement = document.getElementById('word');
+
 const ALL_WORDS = [
   'zara',
   'pelo',
@@ -16,3 +19,20 @@ const ALL_WORDS = [
   'hongo',
   'cordyceps'
 ];
+
+const selectWord = //palabra aleatoria del array
+  ALL_WORDS[Math.floor(Math.random() * ALL_WORDS.length)].toLowerCase();
+//saber el largo de la palabra
+const length = selectWord.length;
+
+const attempts = 5; // INTENTOS const o let ?¿ si tiene que cambiar
+
+const gameBoard = () => {
+  const row = document.createElement('div');
+  const cell = document.createElement('div');
+  for (let intento = 0; intento < attempts; intento++) {
+    //hace hasta 5 jugadas
+    row.classList.add('createRow');
+    for (let letter = 0; letter < length; letra++) {}
+  }
+};
